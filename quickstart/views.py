b8259dbf355e411cc47ba7cd84951f117445be5e
temi1 <william90089@gmail.com>
2022-05-23 11:56:18 +0100

    second commit

--- quickstart/views.py
+++ quickstart/views.py
@@ -15,53 +15,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from datetime import date
 # Create your views here.
-class UploadView(APIView):
-    parser_classes = (
-        MultiPartParser,
-        JSONParser,
-    )
-
-    @staticmethod
-    def post(request):
-        file = request.data.get('picture')
-        upload_data = cloudinary.uploader.upload(file)
-        #print(upload_data)
-        img = upload_data['url']
-
-
-        #load models
-        resnet_chest = ProdModelConfig.model
-
-
-        req = urllib.request.urlopen(img)
-        arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-        image = cv2.imdecode(arr, -1) # 'Load it as it is'
-        #image = cv2.imread('upload_chest.jpg') # read file
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
-        image = cv2.resize(image,(224,224))
-        image = np.array(image) / 255
-        image = np.expand_dims(image, axis=0)
-
-        resnet_pred = resnet_chest.predict(image)
-        probability = resnet_pred[0]
-        #print("Resnet Predictions:")
-        if probability[0] > 0.5:
-            resnet_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID')
-        else:
-            resnet_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
-        #print(resnet_chest_pred)
-        #print(xception_chest_pred)
-        return Response({
-            'status': 'success',
-            'data': upload_data,
-            'url':img,
-            'xception_chest_pred':xception_chest_pred,
-            'inception_chest_pred':inception_chest_pred,
-            'vgg_chest_pred':vgg_chest_pred,
-            'resnet_chest_pred':resnet_chest_pred,
-        }, status=201)
-
-
 
 
 
